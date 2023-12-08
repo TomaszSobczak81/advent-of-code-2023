bb5302a557a9773d69f8c91a1039183653f1c183
tomassob <tomeksobczak@gmail.com>
2023-12-08 01:47:45 +0100

    WIP Day3 solutions

--- src/aoc/day3.rs
+++ src/aoc/day3.rs
@@ -6,14 +6,40 @@ impl crate::aoc::Compute for Day3 {
     fn compute_part_one(&self, version: String) -> String {
         let input = self.input_load("1".to_string(), version.clone());
         let parts = self.collect_parts(&input);
-        let sum: i32 = parts.iter().map(|part| part.number(&input)).sum();
+        let mut sum: i32 = 0;
+
+        for part in parts.iter() {
+            let adjacents: Vec<char> = part.adjacent_cells().iter().map(|c| input[((*c).y, (*c).x)]).collect();
+            
+            match adjacents.iter().any(|c| !c.is_digit(10) && *c != '.') {
+                true => sum += part.value,
+                false => (),
+            }
+        }
 
         sum.to_string()
     }
 
     fn compute_part_two(&self, version: String) -> String {
         let input = self.input_load("2".to_string(), version.clone());
-        "0".to_string()
+        let parts = self.collect_parts(&input);
+        let mut sum: i32 = 0;
+
+        for part in parts.iter() {
+            let adjacents: Vec<(&Cell, char)> = part.adjacent_cells().iter().map(|c| (c, input[((*c).y, (*c).x)])).collect();
+
+            for a in adjacents.iter() {
+                println!("{:?}", a);
+            }
+            // println!("{:?}", adjacents);
+            
+            // match adjacents.iter().any(|c| !c.is_digit(10) && *c == '*') {
+            //     true => sum += part.value,
+            //     false => (),
+            // }
+        }
+
+        sum.to_string()
     }
 }
 
@@ -32,14 +58,14 @@ impl Day3 {
                     value.push(v);
                     cells.push_back(Cell { x: c, y: r });
                 } else if value != "" {
-                    list.push_back(Part { value: value.parse::<i32>().unwrap(), cells: cells });
+                    list.push_back(Part { value: value.parse::<i32>().unwrap(), cells: cells, xymax: Cell { x: grid.num_columns() - 1, y: grid.num_rows() - 1 } });
                     value = "".to_string();
                     cells = LinkedList::new();                
                 }
             }
 
             if value != "" {
-                list.push_back(Part { value: value.parse::<i32>().unwrap(), cells: cells });
+                list.push_back(Part { value: value.parse::<i32>().unwrap(), cells: cells, xymax: Cell { x: grid.num_columns() - 1, y: grid.num_rows() - 1 } });
             }
         }
 
@@ -52,6 +78,7 @@ impl Day3 {
     }
 }
 
+#[derive(Debug)]
 struct Cell {
     x: usize,
     y: usize
@@ -59,62 +86,48 @@ struct Cell {
 
 struct Part {
     value: i32,
-    cells: LinkedList<Cell>
+    cells: LinkedList<Cell>,
+    xymax: Cell
 }
 
 impl Part {
-    fn adjacent_values(&self, grid: &array2d::Array2D<char>) -> Vec<char> {
-        let mut adjacent_values: Vec<char> = Vec::new();
+    fn adjacent_cells(&self) -> Vec<Cell> {
+        let mut cells: Vec<Cell> = Vec::new();
 
         for cell in self.cells.iter() {
             if cell.y > 0 { // north cell
-                adjacent_values.push(grid[(cell.y - 1, cell.x)]);
+                cells.push(Cell {x: cell.x, y: cell.y - 1});
             }
 
-            if cell.y > 0 && cell.x < grid.num_columns() - 1 { // north-east cell
-                adjacent_values.push(grid[(cell.y - 1, cell.x + 1)]);
+            if cell.y > 0 && cell.x < self.xymax.x { // north-east cell
+                cells.push(Cell {x: cell.x + 1, y: cell.y - 1});
             }
 
-            if cell.x < grid.num_columns() - 1 { // east cell
-                adjacent_values.push(grid[(cell.y, cell.x + 1)]);
+            if cell.x < self.xymax.x { // east cell
+                cells.push(Cell {x: cell.x + 1, y: cell.y});
             }
 
-            if cell.y < grid.num_rows() - 1 && cell.x < grid.num_columns() - 1 { // south-east cell
-                adjacent_values.push(grid[(cell.y + 1, cell.x + 1)]);
+            if cell.y < self.xymax.y && cell.x < self.xymax.x { // south-east cell
+                cells.push(Cell {x: cell.x + 1, y: cell.y + 1});
             }
 
-            if cell.y < grid.num_rows() - 1 { // south cell
-                adjacent_values.push(grid[(cell.y + 1, cell.x)]);
+            if cell.y < self.xymax.y { // south cell
+                cells.push(Cell {x: cell.x, y: cell.y + 1});
             }
 
-            if cell.y < grid.num_rows() - 1 && cell.x > 0 { // south-west cell
-                adjacent_values.push(grid[(cell.y + 1, cell.x - 1)]);
+            if cell.y < self.xymax.y && cell.x > 0 { // south-west cell
+                cells.push(Cell {x: cell.x - 1, y: cell.y + 1});
             }
 
             if cell.x > 0 { // west cell
-                adjacent_values.push(grid[(cell.y, cell.x - 1)]);
+                cells.push(Cell {x: cell.x - 1, y: cell.y});
             }
 
             if cell.y > 0 && cell.x > 0 { // north-west cell
-                adjacent_values.push(grid[(cell.y - 1, cell.x - 1)]);
+                cells.push(Cell {x: cell.x - 1, y: cell.y - 1});
             }
         }
 
-        adjacent_values
-    }
-
-    fn is_adjacent(&self, grid: &array2d::Array2D<char>) -> bool {
-        self.adjacent_values(grid).iter().any(|c| !c.is_digit(10) && *c != '.')
-    }
-
-    fn is_part_of_gear(&self, grid: &array2d::Array2D<char>) -> bool {
-        self.adjacent_values(grid).iter().any(|c| !c.is_digit(10) && *c == '*')
-    }
-
-    fn number(&self, grid: &array2d::Array2D<char>) -> i32 {
-        match self.is_adjacent(grid) {
-            true => self.value,
-            false => 0,
-        }
+        cells
     }
 }
